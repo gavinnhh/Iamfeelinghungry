17f50024a01ce9ff9ebd697e1e1ed27691039ed3
GavinHoang <zizhen.huang@sjsu.edu>
2019-09-26 23:43:39 -0700

    fixed sign up and sign in and some Modal revision

--- public/signup/auth.js
+++ public/signup/auth.js
@@ -1,93 +1,60 @@
-// alert("Hi, you are in the sign in page")
-// var fname = $('input[id="firstname"]').val();
 
+
+// var fname = $('input[id="firstname"]').val();
 //var submitBtn = document.querySelector('input[type="button"]');
 // document.getElementById('signupbtn').addEventListener('click', toggleSignUp, false);
 
 function handleSignIn()
 {
     console.log('sign in/out button clicked');
-    // if already signed in, sign in button text has been changed to Sign Out, click that to sign out below here
-    if (firebase.auth().currentUser) {
-        firebase.auth().signOut();
+    var email = document.getElementById('name/email').value;
+    var password = document.getElementById('signin-password').value;
 
-    } else { // if not signed in already, proceed to sign in
-        var email = document.getElementById('email').value;
-        var password = document.getElementById('password').value;
+    // Sign in with email and pass.
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      console.log('signed in!!!');
+    });
 
-        // Sign in with email and pass.
-        // [START authwithemail]
-        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-          console.log('signed in!!!');
-
-        // document.getElementById('home-signin').textContent = 'Sign out';
-        // document.getElementById('home-signup').textContent = email;
-      });
-
-
-      // [END authwithemail]
-    }// end else
+    // Make sign in pop up window go away
+    $('#signInModal').modal('hide');
+    return false;
 }
 
 function handleSignUp()
 {
-  console.log('sign up clicked');
-  var fname = document.getElementById('firstname').value;
-  var lname = document.getElementById('lastname').value;
-  var username = document.getElementById('username').value;
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
+    console.log('sign up clicked');
+    var fname = document.getElementById('firstname').value;
+    var lname = document.getElementById('lastname').value;
+    var username = document.getElementById('username').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
 
-  console.log('fname = ' + fname);
-  firebase.auth().createUserWithEmailAndPassword(email, password).then(cred => {
-                console.log(cred);
+    console.log('fname = ' + fname);
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(cred => {
+                  console.log(cred);
               });
 
-    // var signup = document.getElementById('signupbtn');
-    // if(signup != null){
-    //     console.log(signup.value);
-    // }else{
-    //   console.log('signup is null');
-    // }
-
-    // after sign up, disable this button on home page
-    //document.getElementById('home-signup').disabled = false;
-    window.location.reload();
+    // Make sign up pop up window go away
+    $('#signUpModal').modal('hide');
+    return false;
 }
 
-function handleHomeSignIn()
+function signout()
 {
-  console.log('home sign in clicked');
-    var getValue = $(this).attr("data-target");
-    console.log(getValue);
-    // use this code to stop window pop up
-    // $('#home-signin').attr('data-target','#disabledModal');
-    // var getValue2 = $(this).attr("data-target");
-
+    console.log('home sign in(signout) clicked');
+    // var getValue = $(this).attr("data-target");
+    // console.log(getValue);
     // user signed, Sign Out is showing on home page
     if (firebase.auth().currentUser) {
-        // [START signout]
         firebase.auth().signOut();
-        //$('#home-signin').attr('data-target','#disabledModal');
-
-        // [END signout]
     }
-    //else{
-        // $('#home-signin').attr('data-target','#signInModal');
-        // $('#home-signup').attr('data-target','#signUpModal');
-        // document.getElementById('home-signin').textContent = 'Sign In';
-        // document.getElementById('home-signup').textContent = 'Sign Up';
-    //}
-
 }
 
-function handleHomeSignUp()
+function profile()
 {
-  console.log('home sign up clicked');
-  // if (firebase.auth().currentUser) {
-  //     // TODO: Here we could have another Modal to pop up user profile window
-  //     $('#home-signup').attr('data-target','#disabledModal');
-  // }
+  console.log('home sign up(profile) clicked');
+  // TODO: create a user profile
+
 }
 
 
@@ -127,17 +94,15 @@ function initApp() {
           $('#home-signup').attr('data-target','#signUpModal');
 
         }
-        // [START_EXCLUDE silent]
-        //document.getElementById('home-signin').disabled = false;
-        // [END_EXCLUDE]
+
       });
       // [END authstatelistener]
       document.getElementById('siginbtn').addEventListener('click', handleSignIn, false);
       document.getElementById('signupbtn').addEventListener('click', handleSignUp, false);
-      document.getElementById('home-signin').addEventListener('click', handleHomeSignIn, false);
-      document.getElementById('home-signup').addEventListener('click', handleHomeSignUp, false);
-      // document.getElementById('quickstart-verify-email').addEventListener('click', sendEmailVerification, false);
-      // document.getElementById('quickstart-password-reset').addEventListener('click', sendPasswordReset, false);
+      document.getElementById('home-signin').addEventListener('click', signout, false);
+      document.getElementById('home-signup').addEventListener('click', profile, false);
+      // document.getElementById('verify-email').addEventListener('click', sendEmailVerification, false);
+      // document.getElementById('password-reset').addEventListener('click', sendPasswordReset, false);
 }
 
 window.onload = function() {
