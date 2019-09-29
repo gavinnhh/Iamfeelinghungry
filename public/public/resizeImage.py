504b295bda3e4c3fd37d7fe253429f6d27a15c00
Taylor Krask <kraskt@yahoo.com>
2019-09-29 16:46:56 -0700

    removing duplicate folder

--- public/public/resizeImage.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-from resizeimage import resizeimage
-
-
-with open('food.png', 'r+b') as f:
-    with Image.open(f) as image:
-        cover = resizeimage.resize_cover(image, [180, 180])
-        cover.save('test-image-cover.png', image.format)
