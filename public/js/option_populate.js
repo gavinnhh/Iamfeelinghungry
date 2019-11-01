a24e4bcfc6f6465286bc7d67d11b7c4d2ba4f938
Hieu Tran <hieu.tran@sjsu.edu>
2019-11-01 13:22:06 -0700

    updated dropdown options with js

---
+++ public/js/option_populate.js
@@ -0,0 +1,19 @@
+// retrive data from firebase database
+const database = firebase.firestore();
+const menuDoc = database.collection('dropdown menu').doc("food_tags");
+menuDoc.get().then(function(doc) {
+  var options = doc.data().tags
+  createOption(options);
+});
+
+// populate items in dropdown menu
+function createOption(arr) {
+  var select = document.getElementById("select");
+  for(var i = 0; i < arr.length; i++) {
+    var opt = arr[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    select.appendChild(el);
+  }
+}
