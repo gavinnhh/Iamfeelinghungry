e27a9d22a53dc1d5949a49ac358d10c6e50eb85c
Hieu Tran <hieu.tran@sjsu.edu>
2019-11-07 17:39:38 -0800

    fixed dropdown populate options with js

--- public/js/option_populate.js
+++ public/js/option_populate.js
@@ -1,27 +1,33 @@
-// // retrive data from firebase database
-// const database = firebase.firestore();
-// const menuDoc = database.collection('dropdown menu').doc("food_tags");
-// menuDoc.get().then(function(doc) {
-//   var options = doc.data().tags
-//   createOption(options);
-// });
-
-var arr = ["Chinese", "American", "Vietnamese", "Mexican", "Japanese", "Korean"];
-createOption(arr);
+// retrive data from firebase database
+const database = firebase.firestore();
+const menuDoc = database.collection('dropdown menu').doc("food_tags");
+menuDoc.get().then(function(doc) {
+  var arr = doc.data().tags;
+  createOption(arr);
+});
 
 // populate items in dropdown menu
 function createOption(arr) {
-  console.log("inside");
-  console.log(arr.length);
-  var select = document.getElementById("select");
+  var dddiv = document.getElementById('dropdown-menu');
+  var select = document.createElement('select');
+  select.id = "select";
+  select.multiple = true;
+  select.setAttribute("class", "dropdown-content");
+  dddiv.appendChild(select);
   for(var i = 0; i < arr.length; i++) {
     var opt = arr[i];
-    console.log("opt = " + opt);
     var el = document.createElement("option");
-    var el_text = document.createTextNode(opt);
-    el.appendChild(el_text);
-    //el.textContent = opt;
-    //el.value = opt;
+    el.textContent = opt;
+    el.value = opt;
     select.appendChild(el);
   }
+  tail.select('#select', {
+    search: true,
+    descriptions: true,
+    multilimit: 15,
+    hideSelected: true,
+    hideDisabled: true,
+    multiShowCount: false,
+    multiContainer: '.move-container'
+  });
 }
