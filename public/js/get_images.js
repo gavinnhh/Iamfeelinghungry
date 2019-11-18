41a9f9335fa65044ac8805dadb34a523740beff7
Hieu Tran <hieu.tran@sjsu.edu>
2019-11-17 23:11:58 -0800

    link each result to specific recipe page

--- public/js/get_images.js
+++ public/js/get_images.js
@@ -15,6 +15,26 @@ document.getElementById("img-div2"),
 document.getElementById("img-div5"),
 document.getElementById("img-div3"),
 document.getElementById("img-div6")];
+var info_button = [document.getElementById("button1"),
+document.getElementById("button4"),
+document.getElementById("button2"),
+document.getElementById("button5"),
+document.getElementById("button3"),
+document.getElementById("button6")];
+var userImg = [document.getElementById("userImg1"),
+document.getElementById("userImg4"),
+document.getElementById("userImg2"),
+document.getElementById("userImg5"),
+document.getElementById("userImg3"),
+document.getElementById("userImg6")];
+var username = [document.getElementById("username1"),
+document.getElementById("username4"),
+document.getElementById("username2"),
+document.getElementById("username5"),
+document.getElementById("username3"),
+document.getElementById("username6")];
+var title = [];
+var user = [];
 
 posts.get().then(snapshot => {
   snapshot.forEach(doc => {
@@ -25,6 +45,8 @@ posts.get().then(snapshot => {
         for (var j = 0; j < arr.length; j++) {
           if (chosen_tags[i] == arr[j]) {
             food_urls.push(doc.data().foodUrl);
+            title.push(doc.data().title);
+            user.push(doc.data().fromUser);
             temp_chosen_tag = chosen_tags[i];
             chosen_tags.splice(i ,1);
             chosen_tags.push(temp_chosen_tag);
@@ -39,6 +61,7 @@ posts.get().then(snapshot => {
   });
   for (var i = 0; i < food_urls.length; i++) {
     loadImg(img_divs[i], i);
+    loadImgInfo(info_button[i], userImg[i], username[i], i);
   }
 });
 
@@ -80,4 +103,21 @@ function loadImg(div, index) {
   img.setAttribute("style", "width: 100%");
   img.setAttribute("id", "foodImg");
   imgDiv.prepend(img);
+}
+
+function loadImgInfo(button, user_img, user_name, index) {
+  button.innerText = title[index];
+  button.addEventListener('click', function(){handleViewMore(user[index])}, false);
+  const userDoc = dabase.collection('users').doc(user[index]);
+  userDoc.get().then(function(doc) {
+    user_img.src = doc.data().photoUrl;
+    user_name.innerText = doc.data().username;
+  });
+}
+
+function handleViewMore(postid){
+  console.log("current value: " + postid);
+  console.log("handleViewMore clicked");
+  localStorage.setItem('currentPid', postid); // use localStorage to send postid to recipe.js
+  window.location.href = "../recipe/recipe.html";
 }
